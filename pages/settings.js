8d7a3345408723a051a1bd000e1f15ea91ea5d9e
Petr Brzek <petr@avocode.com>
2019-10-01 22:01:43 +0200

    improved styling in settings

--- pages/settings.js
+++ pages/settings.js
@@ -4,11 +4,13 @@ import Router from "next/router";
 import { useMappedState } from "redux-react-hook";
 
 import { Services } from "../lib/with-services";
+import { getHighestLevel } from '../lib/user-utils'
 
-import Fox from "../static/fox.svg";
 import LeftArrow from "../static/left-arrow.svg";
 
 function Header({ imageSrc }) {
+  const bounds = React.useMemo(() => ({ width: '100px', height: '100px' }), [])
+
   return (
     <div className="sticky top-0 w-full flex items-center overflow-hidden header-bg px-3">
       <div className="flex flex-col">
@@ -21,8 +23,10 @@ function Header({ imageSrc }) {
       <div className="flex flex-col flex-1 justify-center p-2 px-3">
         <h1 className="text-base font-bold leading-tight">Můj profil</h1>
       </div>
-      <div className="flex flex-1 mt-2">
-        <img width="50" src={imageSrc} className="flex ml-auto pt-3" />
+      <div className="flex flex-1">
+        <div className="bg-white p-2 rounded-full flex ml-auto justify-center overflow-hidden" style={bounds}>
+          <img style={{ height: '80px' }} src={imageSrc}  />
+        </div>
       </div>
     </div>
   );
@@ -37,8 +41,9 @@ export default function Settings() {
     []
   );
   const { personalInfo } = useMappedState(mapState);
-  const latestLevel = personalInfo.levels[personalInfo.levels.length - 1];
-  const rankImage = `/static/${latestLevel.rank.toLowerCase()}.svg`;
+  const latestLevel = getHighestLevel(personalInfo.levels);
+
+  const rankImage = `/static/level-${latestLevel.levelId}.svg`;
 
   const handleLogout = async () => {
     const { api } = services;
@@ -51,9 +56,11 @@ export default function Settings() {
       <Header imageSrc={rankImage} />
       <div className="p-3">
         <div className="flex justify-center">
-          <img width="80" src={rankImage} />
+          <img src={rankImage} style={{ height: '140px' }} />
         </div>
-        <button onClick={handleLogout}>Odhlásit</button>
+        <div className="mt-10 text-base">
+          <button onClick={handleLogout} className="font-semibold block p-3 underline text-left w-full">Odhlásit se</button>
+        </div>
       </div>
     </div>
   );
