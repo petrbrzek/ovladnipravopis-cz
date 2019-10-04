0e43f7ee750fbe1c3ee4c255d5b859618b33dadb
Petr Brzek <petr@avocode.com>
2019-10-04 21:05:49 +0200

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import "../static/styles/index.css";
 
 import React from "react";
 import App from "next/app";
+import Router from "next/router";
 import { StoreContext } from "redux-react-hook";
 
 import withUser from "../lib/with-user";
 import withRedux from "../lib/redux/with-redux";
 import { withServices, Services } from "../lib/with-services";
 import Global from "../components/global";
+import * as gtag from "../lib/gtag";
+
+Router.events.on("routeChangeComplete", url => gtag.pageview(url));
 
 class MyApp extends App {
   render() {
