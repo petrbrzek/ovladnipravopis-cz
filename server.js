9a6d1ec9d899ccea74e1808296ceaee574ff9673
Petr Brzek <petr@avocode.com>
2020-08-11 20:32:13 +0200

    remove nextjs from serverjs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const next = require("next");
 const express = require("express");
 const bcrypt = require("bcryptjs");
 
@@ -14,18 +13,16 @@ const mongooseModels = require("./lib/db/models");
 const createConnection = require("./lib/db/connection");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
-app.prepare().then(async () => {
-  const server = express();
+const start = async () => {
+  const app = express();
   const mongooseDb = await createConnection();
 
   const adminBro = new AdminBro({
     databases: [mongooseDb],
     resources: Object.keys(mongooseModels).map((key) => mongooseModels[key]),
   });
+
   const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
     authenticate: async (email, password) => {
       try {
@@ -37,15 +34,17 @@ app.prepare().then(async () => {
       } catch (e) {}
       return false;
     },
-    cookieName: "adminbro", // TODO: save it to env
-    cookiePassword: "secret",
+    cookieName: "adminbro",
+    cookiePassword: process.env.ADMIN_COOKIE_SECRET,
   });
 
-  server.use(adminBro.options.rootPath, router);
-  server.use("/", router);
+  app.use(adminBro.options.rootPath, router);
+  app.use("/", router);
 
-  server.listen(port, (err) => {
+  app.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
-});
+};
+
+start();
